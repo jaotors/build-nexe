e101ab5ba6797fb457ca6a424609120596605a95
Joshua Turingan <turingan.joshua@gmail.com>
2018-12-17 20:53:56 +0800

    update remove index.js

--- sample_index.js
+++ sample_index.js
@@ -3,33 +3,30 @@ const handler = require('serve-handler');
 const kill = require('kill-port');
 const micro = require('micro');
 const { execFile } = require('child_process');
-
-const PORT = 4040;
-const APP_EXE = 'XSplitVCam.exe';
-const PUBLIC_FOLDER = 'build';
+const port = process.env.LOCAL_PORT;
 
 // call after the server is done
 const startExe = () => {
-  console.log(`${APP_EXE} start`);
-  execFile(APP_EXE, (err, data) => {
+  console.log(`${process.env.APP_EXE} start`);
+  execFile(`${process.env.APP_EXE}`, (err, data) => {
     process.exit();
   });
 };
 
 const server = micro(async (req, res) => {
   await handler(req, res, {
-    public: PUBLIC_FOLDER
+    public: `${process.env.PUBLIC_FOLDER}`
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`static server is running at http://localhost:${PORT}`);
+server.listen(port, () => {
+  console.log(`static server is running at http://localhost:${port}`);
   startExe();
 });
 
 // while exit
 process.on('exit', () => {
-  console.log(`http://localhost:${PORT} server will close.`);
+  console.log(`http://localhost:${port} server will close.`);
   // kills the port
   kill(port)
     .then(console.log)
